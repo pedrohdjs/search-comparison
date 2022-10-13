19d2d226881501f7e7a8546fc715735931010c10
jlrconst <jlconsttantino@live.com>
2022-10-13 13:32:19 -0300

    added BFS to Maze class

--- lib/maze.py
+++ lib/maze.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from collections import namedtuple
 import os
 from collections import deque
-from queue import Empty, PriorityQueue
+from queue import PriorityQueue
 import numpy as np
 from time import sleep
 from scipy.spatial.distance import cityblock as get_manhattan_distance
@@ -85,7 +85,6 @@ class Maze:
         self.fruits = self._get_fruits()
         self.done = False
         self.dfs_call_count = 0
-
         self.on_terminal = on_terminal
 
 
@@ -370,3 +369,81 @@ class Maze:
                 self.print()
                 self._maze[pos[0]][pos[1]] = self.VISITED
             self.print()
+        
+        # Finalização
+        self.done = True
+    
+
+    def start_bfs(self) -> True:
+        '''
+        Inicializa o algoritmo de busca BFS, realizando impressão do 
+        resultado a cada caminho calculado.
+        '''
+        # Para controle das frutas (objetivos)
+        fruits = np.array(list(self.fruits))
+        fruits_indexes = np.arange(0, fruits.shape[0])
+        num_fruits = fruits.shape[0]
+        non_collected_fruits = np.full(fruits.shape[0], True)
+        num_collected_fruits = 0
+
+        # Para controle de fluxo
+        Node = namedtuple('Node', ['pos', 'parent'])
+
+        # Para controle de posição
+        current_position = self.current_pos
+
+        # Itera ao longo das frutas
+        while num_collected_fruits < num_fruits:
+
+            # Posição inicial
+            initial_position = (current_position[0], current_position[1])
+
+            # Para controle de fluxo
+            queue = deque([])
+            parents = {}
+            visited = {}
+            
+            # Seleção da fruta mais próxima
+            active_fruits = fruits[non_collected_fruits]
+            active_indexes = fruits_indexes[non_collected_fruits]
+            active_index = 0
+            min_dist = get_manhattan_distance(active_fruits[0], current_position)
+            for i in range(1, active_fruits.shape[0]):
+                dist = get_manhattan_distance(active_fruits[i], current_position)
+                if dist < min_dist:
+                    min_dist = dist
+                    active_index = i
+            current_fruit = tuple(active_fruits[active_index])
+            current_fruit_index = active_indexes[active_index]
+
+            # Inicialização da busca
+            parents[current_position] = initial_position
+            adjacent_node = self._get_valid_positions(visited, current_position)
+            for adjacent_position in adjacent_node:
+                queue.append(Node(adjacent_position, current_position))
+            
+            # Enquanto não finalizar o objetivo atual
+            while current_position != current_fruit and len(queue) > 0:
+                
+                # Obtenção do próximo nó
+                current_node = queue.pop()
+                current_position = current_node.pos
+                parents[current_position] = current_node.parent
+                visited[current_position] = True
+
+                # Impressão
+                self._maze[current_position[0]][current_position[1]] = self.PACMAN
+                self.print()
+                self._maze[current_position[0]][current_position[1]] = self.VISITED
+
+                # Adição dos nós adjacentes à fila
+                adjacent_node = self._get_valid_positions(visited, current_position)
+                for adjacent_position in adjacent_node:
+                    queue.append(Node(adjacent_position, current_position))
+                
+            # Atualização de objetivo
+            num_collected_fruits += 1
+            non_collected_fruits[current_fruit_index] = False
+        
+        # Finalização
+        self.done = True
